cfb2cecd145284db322d44f56314bc53d9e6fc02
David Liendo <davidals70@gmail.com>
2023-01-17 21:51:31 -0400

    Ligero ajuste

--- flutter_app_1/lib/aplicacion/servicios/servicio_almacenar_info_curso_profesor_local.dart
+++ flutter_app_1/lib/aplicacion/servicios/servicio_almacenar_info_curso_profesor_local.dart
@@ -1,16 +1,13 @@
 import 'package:flutter_pantalla_1/aplicacion/parameter_objects/parametro_adaptador_iterable.dart';
-import 'package:flutter_pantalla_1/aplicacion/patron_decorador/i_servicio.dart';
 import 'package:flutter_pantalla_1/infraestructura/data/modelo_temporal/usuario_temp.dart';
-import 'package:flutter_pantalla_1/modelos/result.dart';
 import '../../dominio/parameters_objects/info_curso_con_profesor.dart';
 import '../../modelos/patron_iterador/iterado_generico/iterable_lista.dart';
 import '../../modelos/patron_iterador/iterado_generico/iterador_lista.dart';
 import '../../infraestructura/data/Adaptador/i_repository_bd.dart';
 import '../../infraestructura/data/modelo_temporal/curso_temp.dart';
 
-class ServicioGuardarInfoCursoProfesorDBMejorado implements IService<bool, ParametroAdaptadorIterable<IRepositorioMoor, IterableLista<InfoCursoConProfesor>>>{
-  @override
-  Future<Result<bool>> execute(ParametroAdaptadorIterable<IRepositorioMoor, IterableLista<InfoCursoConProfesor>> parametro) async {
+class ServicioGuardarInfoCursoProfesorDBMejorado{
+  Future<void> execute(ParametroAdaptadorIterable<IRepositorioMoor, IterableLista<InfoCursoConProfesor>> parametro) async {
     IRepositorioMoor adaptadorCursoProfesor = parametro.getAdaptador();
     IterableLista<InfoCursoConProfesor>? datos = parametro.getIterable();
     IteradorLista<InfoCursoConProfesor> iteradorDatos = datos.crearIterador();
@@ -20,9 +17,6 @@ class ServicioGuardarInfoCursoProfesorDBMejorado implements IService<bool, Param
     iteradorDatos.resetPosicionInicial();
     adaptadorCursoProfesor.guardarTodosLosUsuarios(traducirProfesores(iteradorDatos));
     adaptadorCursoProfesor.close();
-
-    Result<bool> nulo = Result();
-    return nulo;
   }
 
   List<CursoTemp> traducirCursos(IteradorLista<InfoCursoConProfesor> cursos) {
